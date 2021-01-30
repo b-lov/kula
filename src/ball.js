4bea932a971f534a326814f61828737c5fd8d85f
maxpiano <maxbilov@gmail.com>
2021-01-31 00:36:37 +0100

    ball moving

--- src/ball.js
+++ src/ball.js
@@ -1,26 +1,34 @@
 import * as THREE from 'three';
 
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// init scene
 const scene = new THREE.Scene();
+
+// init camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.SphereGeometry(0.6, 32, 32);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const ball = new THREE.Mesh(geometry, material);
-scene.add(ball);
 camera.position.z = 5;
 
+// ball
+const geometry = new THREE.SphereGeometry(0.6, 16, 16);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xa2a7a9,
+  wireframe: true,
+});
+const ball = new THREE.Mesh(geometry, material);
+scene.add(ball);
+
+// controls
 var xSpeed = 0.01;
 var ySpeed = 0.01;
-
 document.addEventListener('keydown', onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
   var keyCode = event.which;
@@ -32,9 +40,10 @@ function onDocumentKeyDown(event) {
     ball.position.x -= xSpeed;
   } else if (keyCode == 68) {
     ball.position.x += xSpeed;
-  } else if (keyCode == 32) {
+  } else if (keyCode == 16) {
     ball.position.set(0, 0, 0);
   }
+  renderer.render(scene, camera);
 }
 
 function animate() {
